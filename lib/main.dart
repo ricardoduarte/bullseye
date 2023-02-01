3e6f5044472d32ec47173769f2e140a3fecee1f9
Ricardo Duarte <rickduarte@gmail.com>
2023-02-01 10:06:18 -0300

    implement button and text on main page

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,42 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: 'Bullseye',
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Bullseye'),
-      ),
-      body: const Center(
-        child: Text('Hello Bullseye!'),
-      ),
-    ),
+    home: GamePage(),
   ));
+}
+
+class GamePage extends StatefulWidget {
+  const GamePage({super.key});
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Hello Bullseye',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.green,
+              ),
+            ),
+            TextButton(
+              child:
+                  const Text('Hit me!', style: TextStyle(color: Colors.blue)),
+              onPressed: () {
+                print('Hello');
+              }
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
